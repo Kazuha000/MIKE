f352662f78e0ab5cc452bb78be82c998867c846c
Kazuha000 <2078501297@qq.com>
2023-07-26 22:30:50 +0800

    0725

---
+++ src/main/java/com/cqupt/mike/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.cqupt.mike.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class TestController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello,spring boot!";
+    }
+
+
+    @GetMapping("/thymeleaf")
+    public String hello(HttpServletRequest request, @RequestParam(value = "description", required = false, defaultValue = "springboot-thymeleaf") String description) {
+        request.setAttribute("description", description);
+        return "thymeleaf";
+    }
+
+}
