92f4c41b29038dae687d56490bdac43117f54891
Kazuha000 <2078501297@qq.com>
2023-07-26 09:19:17 +0800

    0725

--- src/main/java/com/cqupt/mike/controller/admin/AdminController.java
+++ src/main/java/com/cqupt/mike/controller/admin/AdminController.java
@@ -1,4 +1,14 @@
 package com.cqupt.mike.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class AdminController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello,spring boot!";
+    }
 }
