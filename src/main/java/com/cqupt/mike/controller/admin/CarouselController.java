4c17a610d241cffa470cbc56cd925328608d51d8
Kazuha000 <2078501297@qq.com>
2023-08-01 17:08:22 +0800

    0725

--- src/main/java/com/cqupt/mike/controller/admin/CarouselController.java
+++ src/main/java/com/cqupt/mike/controller/admin/CarouselController.java
@@ -1,10 +1,19 @@
 package com.cqupt.mike.controller.admin;
 
+import com.cqupt.mike.common.ServiceResultEnum;
+import com.cqupt.mike.entity.Carousel;
+import com.cqupt.mike.service.CarouselService;
+import com.cqupt.mike.until.PageQueryUtil;
+import com.cqupt.mike.until.Result;
+import com.cqupt.mike.until.ResultGenerator;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 轮播图控制
@@ -12,9 +21,92 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/admin")
 public class CarouselController {
+
+    @Resource
+    CarouselService carouselService;
     @GetMapping("/carousels")
     public String carouselPage(HttpServletRequest request) {
         request.setAttribute("path", "mike_carousel");
         return "admin/mike_carousel";
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping(value = "/carousels/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Result list(@RequestParam Map<String, Object> params) {
+        if (StringUtils.isEmpty(params.get("page")) || StringUtils.isEmpty(params.get("limit"))) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(carouselService.getCarouselPage(pageUtil));
+    }
+
+    /**
+     * 添加
+     */
+    @RequestMapping(value = "/carousels/save", method = RequestMethod.POST)
+    @ResponseBody
+    public Result save(@RequestBody Carousel carousel) {
+        if (StringUtils.isEmpty(carousel.getCarouselUrl())
+                || Objects.isNull(carousel.getCarouselRank())) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = carouselService.saveCarousel(carousel);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+
+    /**
+     * 修改
+     */
+    @RequestMapping(value = "/carousels/update", method = RequestMethod.POST)
+    @ResponseBody
+    public Result update(@RequestBody Carousel carousel) {
+        if (Objects.isNull(carousel.getCarouselId())
+                || StringUtils.isEmpty(carousel.getCarouselUrl())
+                || Objects.isNull(carousel.getCarouselRank())) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = carouselService.updateCarousel(carousel);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+    /**
+     * 详情
+     */
+    @GetMapping("/carousels/info/{id}")
+    @ResponseBody
+    public Result info(@PathVariable("id") Integer id) {
+        Carousel carousel = carouselService.getCarouselById(id);
+        if (carousel == null) {
+            return ResultGenerator.genFailResult(ServiceResultEnum.DATA_NOT_EXIST.getResult());
+        }
+        return ResultGenerator.genSuccessResult(carousel);
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping(value = "/carousels/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public Result delete(@RequestBody Integer[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        if (carouselService.deleteBatch(ids)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("删除失败");
+        }
     }
 }
