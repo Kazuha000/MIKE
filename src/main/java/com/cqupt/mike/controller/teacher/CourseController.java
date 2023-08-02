e44c13425e46e45901cd909b62bfbd05d86a739f
Kazuha000 <2078501297@qq.com>
2023-08-02 16:18:17 +0800

    0725

--- src/main/java/com/cqupt/mike/controller/teacher/CourseController.java
+++ src/main/java/com/cqupt/mike/controller/teacher/CourseController.java
@@ -1,37 +1,43 @@
 package com.cqupt.mike.controller.teacher;
 
+import com.cqupt.mike.common.CategoryLevelEnum;
+import com.cqupt.mike.entity.CourseCategory;
+import com.cqupt.mike.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/teacher")
 public class CourseController {
 
-//    @Resource
-////    private CategoryService categoryService;
-//
-//    @GetMapping("/course/edit")
-//    public String edit(HttpServletRequest request) {
-//        request.setAttribute("path", "edit");
-//        //查询所有的一级分类
-//       /* List<GoodsCategory> firstLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(0L), NewBeeMallCategoryLevelEnum.LEVEL_ONE.getLevel());
-//        if (!CollectionUtils.isEmpty(firstLevelCategories)) {
-//            //查询一级分类列表中第一个实体的所有二级分类
-//            List<GoodsCategory> secondLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(firstLevelCategories.get(0).getCategoryId()), NewBeeMallCategoryLevelEnum.LEVEL_TWO.getLevel());
-//            if (!CollectionUtils.isEmpty(secondLevelCategories)) {
-//                //查询二级分类列表中第一个实体的所有三级分类
-//                List<GoodsCategory> thirdLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(secondLevelCategories.get(0).getCategoryId()), NewBeeMallCategoryLevelEnum.LEVEL_THREE.getLevel());
-//                request.setAttribute("firstLevelCategories", firstLevelCategories);
-//                request.setAttribute("secondLevelCategories", secondLevelCategories);
-//                request.setAttribute("thirdLevelCategories", thirdLevelCategories);
-//                request.setAttribute("path", "course-edit");
-//                return "teacher/course_edit";
-//            }
-//        }*/
-//        return "error/error_5xx";
-//    }
+    @Resource
+    private CategoryService categoryService;
+
+    @GetMapping("/course/edit")
+    public String edit(HttpServletRequest request) {
+        request.setAttribute("path", "edit");
+        //查询所有的一级分类
+        List<CourseCategory> firstLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(0L), CategoryLevelEnum.LEVEL_ONE.getLevel());
+        if (!CollectionUtils.isEmpty(firstLevelCategories)) {
+            //查询一级分类列表中第一个实体的所有二级分类
+            List<CourseCategory> secondLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(firstLevelCategories.get(0).getCategoryId()), CategoryLevelEnum.LEVEL_TWO.getLevel());
+            if (!CollectionUtils.isEmpty(secondLevelCategories)) {
+                //查询二级分类列表中第一个实体的所有三级分类
+                List<CourseCategory> thirdLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(secondLevelCategories.get(0).getCategoryId()), CategoryLevelEnum.LEVEL_THREE.getLevel());
+                request.setAttribute("firstLevelCategories", firstLevelCategories);
+                request.setAttribute("secondLevelCategories", secondLevelCategories);
+                request.setAttribute("thirdLevelCategories", thirdLevelCategories);
+                request.setAttribute("path", "course-edit");
+                return "teacher/course_edit";
+            }
+        }
+        return "error/error_5xx";
+    }
 }
