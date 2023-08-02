ad4b19d0a90e9d1b33a8db0b970708de7cb7d3c6
Kazuha000 <2078501297@qq.com>
2023-08-02 15:49:24 +0800

    0725

--- src/main/java/com/cqupt/mike/controller/teacher/CourseController.java
+++ src/main/java/com/cqupt/mike/controller/teacher/CourseController.java
@@ -11,27 +11,27 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/teacher")
 public class CourseController {
 
-    @Resource
-//    private CategoryService categoryService;
-
-    @GetMapping("/course/edit")
-    public String edit(HttpServletRequest request) {
-        request.setAttribute("path", "edit");
-        //查询所有的一级分类
-       /* List<GoodsCategory> firstLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(0L), NewBeeMallCategoryLevelEnum.LEVEL_ONE.getLevel());
-        if (!CollectionUtils.isEmpty(firstLevelCategories)) {
-            //查询一级分类列表中第一个实体的所有二级分类
-            List<GoodsCategory> secondLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(firstLevelCategories.get(0).getCategoryId()), NewBeeMallCategoryLevelEnum.LEVEL_TWO.getLevel());
-            if (!CollectionUtils.isEmpty(secondLevelCategories)) {
-                //查询二级分类列表中第一个实体的所有三级分类
-                List<GoodsCategory> thirdLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(secondLevelCategories.get(0).getCategoryId()), NewBeeMallCategoryLevelEnum.LEVEL_THREE.getLevel());
-                request.setAttribute("firstLevelCategories", firstLevelCategories);
-                request.setAttribute("secondLevelCategories", secondLevelCategories);
-                request.setAttribute("thirdLevelCategories", thirdLevelCategories);
-                request.setAttribute("path", "course-edit");
-                return "teacher/course_edit";
-            }
-        }*/
-        return "error/error_5xx";
-    }
+//    @Resource
+////    private CategoryService categoryService;
+//
+//    @GetMapping("/course/edit")
+//    public String edit(HttpServletRequest request) {
+//        request.setAttribute("path", "edit");
+//        //查询所有的一级分类
+//       /* List<GoodsCategory> firstLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(0L), NewBeeMallCategoryLevelEnum.LEVEL_ONE.getLevel());
+//        if (!CollectionUtils.isEmpty(firstLevelCategories)) {
+//            //查询一级分类列表中第一个实体的所有二级分类
+//            List<GoodsCategory> secondLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(firstLevelCategories.get(0).getCategoryId()), NewBeeMallCategoryLevelEnum.LEVEL_TWO.getLevel());
+//            if (!CollectionUtils.isEmpty(secondLevelCategories)) {
+//                //查询二级分类列表中第一个实体的所有三级分类
+//                List<GoodsCategory> thirdLevelCategories = categoryService.selectByLevelAndParentIdsAndNumber(Collections.singletonList(secondLevelCategories.get(0).getCategoryId()), NewBeeMallCategoryLevelEnum.LEVEL_THREE.getLevel());
+//                request.setAttribute("firstLevelCategories", firstLevelCategories);
+//                request.setAttribute("secondLevelCategories", secondLevelCategories);
+//                request.setAttribute("thirdLevelCategories", thirdLevelCategories);
+//                request.setAttribute("path", "course-edit");
+//                return "teacher/course_edit";
+//            }
+//        }*/
+//        return "error/error_5xx";
+//    }
 }
