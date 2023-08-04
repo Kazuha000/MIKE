db9d8d4fce0e32398e3e82991602c84341bebff7
Kazuha000 <2078501297@qq.com>
2023-08-04 15:11:27 +0800

    课程管理

--- src/main/java/com/cqupt/mike/dao/CourseMapper.java
+++ src/main/java/com/cqupt/mike/dao/CourseMapper.java
@@ -2,6 +2,11 @@ package com.cqupt.mike.dao;
 
 import com.cqupt.mike.entity.Carousel;
 import com.cqupt.mike.entity.Course;
+import com.cqupt.mike.util.PageQueryUtil;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 //课程管理
 public interface CourseMapper {
 
@@ -11,4 +16,34 @@ public interface CourseMapper {
      * @return
      */
     int insertSelective(Course record);
+
+    /**
+     * 查询分页数据
+     * @param pageUtil
+     * @return
+     */
+    List<Course> findCourseList(PageQueryUtil pageUtil);
+
+    /**
+     * 查询总数
+     * @param pageUtil
+     * @return
+     */
+    int getTotalCourse(PageQueryUtil pageUtil);
+
+    /**
+     * 批量修改记录
+     * @param courseIds
+     * @param sellStatus
+     * @return
+     */
+    int batchUpdateSellStatus(@Param("courseIds")Long[] courseIds, @Param("sellStatus") int sellStatus);
+
+    /**
+     * 通过id查询课程
+     * @param courseId
+     * @return
+     */
+    Course selectByPrimaryKey(Long courseId);
+
 }
